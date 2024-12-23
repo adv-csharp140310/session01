7c3cf568ddf904be8d76a2f23d82bb26a5612988
Pashaie <pashaie@gmail.com>
2024-12-23 19:23:39 +0330

    refactor Product

--- session01/Form1.cs
+++ session01/Form1.cs
@@ -117,21 +117,4 @@
         }
     }
 
-    class Product 
-    {
-        public string Name { get; set; }
-        public int Price { get; set; }
-        public int Rate { get; set; }
-
-        public Product()
-        {
-            
-        }
-        public Product(string Name, int Price)
-        {
-            this.Name = Name;
-            this.Price = Price; 
-        }
-    }
-
 }
---
+++ session01/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace session01;
+
+class Product 
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public int Rate { get; set; }
+
+    public Product()
+    {
+        
+    }
+    public Product(string Name, int Price)
+    {
+        this.Name = Name;
+        this.Price = Price; 
+    }
+}
