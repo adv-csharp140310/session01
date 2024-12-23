27fa36985417c4bac037d5d91dc5ecf87906f9b8
Pashaie <pashaie@gmail.com>
2024-12-23 20:41:09 +0330

    calc with enum

--- session01/UI/FormCalc.cs
+++ session01/UI/FormCalc.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace session01.UI;
+﻿namespace session01.UI;
 public partial class FormCalc : Form
 {
     public FormCalc()
@@ -18,22 +8,22 @@ public partial class FormCalc : Form
 
     private void buttonAdd_Click(object sender, EventArgs e)
     {
-        textBoxResult.Text = Calc(Convert.ToDouble(textBoxA.Text), Convert.ToDouble(textBoxB.Text), "+").ToString();
+        textBoxResult.Text = Calc(Convert.ToDouble(textBoxA.Text), Convert.ToDouble(textBoxB.Text), CalcOp.ADD).ToString();
     }
 
     private void buttonSub_Click(object sender, EventArgs e)
     {
-        textBoxResult.Text = Calc(Convert.ToDouble(textBoxA.Text), Convert.ToDouble(textBoxB.Text), "-").ToString();
+        textBoxResult.Text = Calc(Convert.ToDouble(textBoxA.Text), Convert.ToDouble(textBoxB.Text), CalcOp.SUB).ToString();
     }
 
     private void buttonMul_Click(object sender, EventArgs e)
     {
-        textBoxResult.Text = Calc(Convert.ToDouble(textBoxA.Text), Convert.ToDouble(textBoxB.Text), "*").ToString();
+        textBoxResult.Text = Calc(Convert.ToDouble(textBoxA.Text), Convert.ToDouble(textBoxB.Text), CalcOp.MUL).ToString();
     }
 
     private void buttonDiv_Click(object sender, EventArgs e)
     {
-        textBoxResult.Text = Calc(Convert.ToDouble(textBoxA.Text), Convert.ToDouble(textBoxB.Text), "/").ToString();
+        textBoxResult.Text = Calc(Convert.ToDouble(textBoxA.Text), Convert.ToDouble(textBoxB.Text), CalcOp.DIV).ToString();
     }
 
     private double Calc(double a, double b, string op)
@@ -59,4 +49,38 @@ public partial class FormCalc : Form
         }
         return result;
     }
+
+    private double Calc(double a, double b, CalcOp op)
+    {
+        double result = 0;
+        switch (op)
+        {
+            case CalcOp.ADD:
+                result = a + b;
+                break;
+            case CalcOp.SUB:
+                result = a - b;
+                break;
+            case CalcOp.MUL:
+                result = a * b;
+                break;
+            case CalcOp.DIV:
+                result = a / b;
+                break;
+            default:
+                MessageBox.Show("Op is not defined");
+                throw new Exception();
+
+        }
+        return result;
+    }
+}
+
+
+enum CalcOp
+{
+    ADD,
+    SUB, 
+    MUL,
+    DIV
 }
