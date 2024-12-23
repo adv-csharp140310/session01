9a8dc815a4d0404a32cca6e453baac7ba9fe7382
Pashaie <pashaie@gmail.com>
2024-12-23 20:06:30 +0330

    .

--- session01/Product.cs
+++ session01/Product.cs
@@ -1,11 +1,22 @@
 ﻿namespace session01;
 
-class Product 
+/*
+ Accessibility
+    Public
+    Internal
+    
+*/
+public class Product 
 {
+    //fields
+    private int _id;   
+    
+    //property
     public string Name { get; set; }
     public int Price { get; set; }
     public int Rate { get; set; }
 
+    //Constractor
     public Product()
     {
         
@@ -15,4 +26,34 @@ class Product
         this.Name = Name;
         this.Price = Price; 
     }
+
+    //method
+    public int GetPrice()
+    {
+        return this.Price;
+    }
+
+
+    public void OverLoad()
+    {
+
+    }
+    public void OverLoad(int a)
+    {
+
+    }
+
+    public void OverLoad(string a)
+    {
+
+    }
+
+
+    public void Calc(double a, double b, string op)
+    {
+
+    }
+
+
+    //delegate/event
 }
